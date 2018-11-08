0424843c7749e6dfb958c36cb7db2c4825107c30
Alex <adivivi@gmx.de>
2018-11-08 17:02:42 +0100

    github pages

---
+++ docs/precache-manifest.d863112ce1d87202767335c53da92d6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "27b06b753025d3e7cdcb",
+    "url": "/static/js/main.27b06b75.chunk.js"
+  },
+  {
+    "revision": "f4926859e068d12e2815",
+    "url": "/static/js/1.f4926859.chunk.js"
+  },
+  {
+    "revision": "27b06b753025d3e7cdcb",
+    "url": "/static/css/main.8c20ff95.chunk.css"
+  },
+  {
+    "revision": "26c65ba013e3e4e1cb339d6d5e58ce42",
+    "url": "/index.html"
+  }
+];
